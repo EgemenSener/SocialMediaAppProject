acc93d904ef7f461c6a0dc4b967ad5e791ac822e
Egemen <egemensener98@gmail.com>
2022-05-17 13:18:54 +0300

    fetching profile data from rest api

--- routes/posts.js
+++ routes/posts.js
@@ -71,9 +71,9 @@ router.get("/:id", async (req, res) => {
 });
 
 //Get timeline posts
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId); //get current user id
+    const currentUser = await User.findById(req.params.userId); //get current user id
     const userPosts = await Post.find({ userId: currentUser._id }); // get current user all posts
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
@@ -85,4 +85,16 @@ router.get("/timeline/all", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//Get users all post
+router.get("/profile/:username", async (req,res) => {
+  try {
+    const user = await User.findOne({username: req.params.username});
+    const posts = await Post.find({userId : user._id});
+    res.status(200).json(posts);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 module.exports = router;
