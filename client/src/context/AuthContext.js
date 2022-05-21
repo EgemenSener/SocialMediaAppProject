e5b16f8cbb6aeabdd919cd3948ec79fa511b3a99
Egemen <egemensener98@gmail.com>
2022-05-21 22:26:45 +0300

    calling current user

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,24 +2,34 @@ import { createContext, useReducer } from "react";
 import { AuthReducer } from "./AuthReducer";
 
 const INITIAL_STATE = {
-    user : null,
-    isFetching : false,
-    error : false
-}
+  user: {
+    _id: "627d083f5c663b676b4864ff",
+    username: "egemen",
+    email: "egemen@gmail.com",
+    profilePicture: "person/1.jpeg",
+    coverPicture: "",
+    isAdmin: false,
+    followers: [],
+    followings: [],
+  },
+  isFetching: false,
+  error: false,
+};
 
 export const AuthContext = createContext(INITIAL_STATE);
-export const AuthContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-    return (
-        <AuthContext.Provider value={{
-            user : state.user,
-            isFetching : state.isFetching,
-            error : state.error,
-            dispatch
-        }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
