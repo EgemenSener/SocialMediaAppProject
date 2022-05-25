800de99349703281c40c3b722ee8f657bbb276d8
Egemen <egemensener98@gmail.com>
2022-05-25 19:13:39 +0300

    fixing profile pic problem

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
 import axios from "axios";
-import { SettingsInputCompositeSharp } from "@material-ui/icons";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Feed({ username }) {
+  const { user } = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
         const res = username ? 
-        await axios.get(`/posts/profile/${username}`) :
-        await axios.get("/posts/timeline/627be5d034fef33231abd3fb") 
+        await axios.get("/posts/profile/" + username) :
+        await axios.get("/posts/timeline/" + user._id) 
         setPosts(res.data.sort((a,b) => {
            return new Date(b.createdAt) - new Date(a.createdAt)}));
     }
